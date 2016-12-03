86ddedd1737b368d6952f5afab1de3d462f83011
apobits <apobits@gmail.com>
2016-12-03 16:31:04 -0500

    third commit

--- src/ortiz/perez/albin/reader/Reader.java
+++ src/ortiz/perez/albin/reader/Reader.java
@@ -3,10 +3,27 @@
  */
 package ortiz.perez.albin.reader;
 
+import java.util.Scanner;
+
 /**
  * @author Administrador
  *
  */
 public class Reader {
+
+	public void read() {
+		Scanner scanner = new Scanner(System.in);
+		while (scanner.hasNext()) {
+			System.out.println(scanner.nextLine());
+			scanner.close();
+		}
+		
+
+	}
+
+	public static void main(String args[]) {
+		Reader reader = new Reader();
+		reader.read();
+	}
 
 }
---
+++ src/ortiz/perez/albin/reader/Test.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package ortiz.perez.albin.reader;
+
+/**
+ * @author Administrador
+ *
+ */
+public class Test {
+	
+	public static void main(String args[]){
+		
+	}
+
+}
