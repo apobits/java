4920a2b85dc6310201ab45092c1c365489450241
apobits <apobits@gmail.com>
2016-12-22 21:26:25 -0500

    testing

--- src/ortiz/perez/albin/reader/DataBase.java
+++ src/ortiz/perez/albin/reader/DataBase.java
@@ -4,6 +4,7 @@
 package ortiz.perez.albin.reader;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -14,29 +15,42 @@ import oracle.jdbc.pool.OracleDataSource;
  *
  */
 public class DataBase {
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
+	
+	public void useDriverManager(){
 		try {
-			OracleDataSource ods = new OracleDataSource();
-			ods.setPortNumber(1521);
-			ods.setServerName("localhost");
-			ods.setDatabaseName("apo");
-			ods.setDriverType("thin");
-			ods.setUser("apo");
-			ods.setPassword("apo51331605");
-			ods.setServiceName("xe");
-			Connection conn = ods.getConnection();
+			Connection conn = DriverManager.getConnection("jdbc:oracle:thin:@//localhost:1521/xe", "apo", "apo51331605");
 			Statement st = conn.createStatement();
 			st.executeQuery("insert into table1 values ('lol')");
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		} finally {
-
 		}
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		DataBase db = new DataBase();
+		db.useDriverManager();
+//		try {
+//			OracleDataSource ods = new OracleDataSource();
+//			ods.setPortNumber(1521);
+//			ods.setServerName("localhost");
+//			ods.setDatabaseName("apo");
+//			ods.setDriverType("thin");
+//			ods.setUser("apo");
+//			ods.setPassword("apo51331605");
+//			ods.setServiceName("xe");
+//			Connection conn = ods.getConnection();
+//			Statement st = conn.createStatement();
+//			st.executeQuery("insert into table1 values ('lol')");
+//		} catch (SQLException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		} finally {
+//
+//		}
 
 	}
 
