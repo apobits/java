02074726390be453e5ea7ba228a76369145bbada
apobits <apobits@gmail.com>
2017-09-06 23:01:14 -0500

    commit

--- JavaPractice/src/CodingBat.java
+++ JavaPractice/src/CodingBat.java
@@ -8,4 +8,16 @@
  */
 public class CodingBat {
 
+    public static String stringBits(String str) {
+	StringBuilder sb = new StringBuilder();
+	for (int i = 0; i < str.length(); i+=2) {
+		sb.append(str.charAt(i));
+	}
+	return sb.toString();
+    }
+
+    public static void main(String args[]) {
+	System.out.println(stringBits("Heeololeo"));
+    }
+
 }
