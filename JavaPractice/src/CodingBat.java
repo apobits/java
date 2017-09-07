7f6fd4b0618842dbe061a5aafb8578168d58bdab
apobits <apobits@gmail.com>
2017-09-06 21:43:13 -0500

    commit

--- JavaPractice/src/CodingBat.java
+++ JavaPractice/src/CodingBat.java
@@ -8,47 +8,4 @@
  */
 public class CodingBat {
 
-<<<<<<< HEAD
-    static int countXX(String str) {
-	int count = 0;
-	double temp = 0;
-	for (int i = 0; i < str.length(); i++) {
-	    if (str.charAt(i) == 'x')
-		temp++;
-	    else if (temp == 2) {
-		count +=1;
-		temp = 0;
-	    }else if(temp>2){
-		count +=temp-1;
-	    }
-
-	    if (i == str.length() - 1 && temp > 0)
-		if (temp == 2) {
-			count +=1;
-			temp = 0;
-		    }else if(temp>2){
-			count +=temp-1;
-		    }
-	}
-
-	return count;
-    }
-
-    public static void main(String[] args) {
-	System.out.println(countXX("xxxxx"));
-
-    }
-=======
-	static boolean countXX(String str) {
-		int index = str.indexOf("x");
-
-		return (str.length() - 1 != index) && (str.charAt(index + 1) == 'x') ? true : false;
-	}
-
-	public static void main(String[] args) {
-		String x = "HixxElxxyxxxnxx";
-		System.out.println(countXX(x));
-	}
->>>>>>> 35801a0c045c04d3a2e49368cd24e928f1c2ed41
-
 }
