4344295a5062642d98dd7afa27eee3c6a8dc17ca
aposo <aposo@DESKTOP-M6GP2R6>
2019-01-03 14:49:57 -0500

    updated

--- JavaPractice/src/ortiz/perez/albin/auto/boxin_unboxing/Boxing.java
+++ JavaPractice/src/ortiz/perez/albin/auto/boxin_unboxing/Boxing.java
@@ -9,20 +9,21 @@ package ortiz.perez.albin.auto.boxin_unboxing;
  */
 public class Boxing {
 
-    /**
-     * @param args
-     */
-    public static void main(String[] args) {
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
 
-	int a = 10, b = 15;
-	Integer x = a;// autboxing, Java implicitly creates the new Integer object with the value that a holds
-	System.out.println("Primitive type " + a + " \nInteger type " + x);
+		int a = 10, b = 15;
+		Integer x = a;// autboxing, Java implicitly creates the new Integer object with the value that
+						// a holds
+		System.out.println("Primitive type " + a + " \nInteger type " + x);
 
-	int y = x + b;// auto unboxing, Java implicitly obtains the int value from the Integer object
-	System.out.println(y);
+		int y = x + b;// auto unboxing, Java implicitly obtains the int value from the Integer object
+		System.out.println(y);
 
-	System.out.println("Incrementing x object ");
-	System.out.println(++x);
-    }
+		System.out.println("Incrementing x object ");
+		System.out.println(++x);
+	}
 
 }
