19a0e6a1e8b4efaa1188a83393aec3279a625123
apobits <apobits@gmail.com>
2019-03-27 17:54:49 -0500

    updated

--- JavaPractice/src/practice/java/nio/DailyPracticeExample.java
+++ JavaPractice/src/practice/java/nio/DailyPracticeExample.java
@@ -3,20 +3,50 @@
  */
 package practice.java.nio;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
+
+import com.sun.org.apache.xerces.internal.impl.dv.util.Base64;
+
 /**
  * @author apobits@gmail.com
  *
  */
 public class DailyPracticeExample {
 	public static void main(String[] args) {
-		for (String arg : args) {
-			switch (arg) {
-			case "a":
-				System.out.println("great!");
-				break;
-			default:
-				System.out.println("unknown");
+		try {
+			URL url = new URL("https://qa.api.tigo.com/oauth/client_credential/accesstoken");
+			HttpsURLConnection connection = (HttpsURLConnection) url.openConnection();
+			connection.setRequestMethod("GET");
+			connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+			connection.setRequestProperty("Autorization",
+					"Basic " + Base64.encode("FMkOk7RUZaNLX6LPVGi6LjsZfBvq8UPu:ALOG1TPrz6hoPmvz".getBytes()));
+			connection.setRequestProperty("Accept", "application/json");
+			connection.setDoOutput(true);
+
+			try (BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(connection.getOutputStream()))) {
+
+				bw.write("grant_type=client_credentials");
+				bw.flush();
+
 			}
+
+			try (BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));) {
+
+				String line = null;
+				while ((line = br.readLine()) != null) {
+					System.out.println(line);
+				}
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 	}
 }
