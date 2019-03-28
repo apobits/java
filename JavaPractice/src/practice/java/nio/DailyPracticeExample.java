46fc246f3217bc8a62322bffe08a3cf251dc01a2
aposo <aposo@DESKTOP-M6GP2R6>
2019-03-28 12:44:18 -0500

    udpated

--- JavaPractice/src/practice/java/nio/DailyPracticeExample.java
+++ JavaPractice/src/practice/java/nio/DailyPracticeExample.java
@@ -9,24 +9,25 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.URL;
+import java.util.Base64;
 
 import javax.net.ssl.HttpsURLConnection;
-
-import com.sun.org.apache.xerces.internal.impl.dv.util.Base64;
 
 /**
  * @author apobits@gmail.com
  *
  */
 public class DailyPracticeExample {
-	public static void main(String[] args) {
+
+	public static void getAccessToken() {
 		try {
-			URL url = new URL("https://qa.api.tigo.com/oauth/client_credential/accesstoken");
+			URL url = new URL(
+					"https://qa.api.tigo.com/oauth/client_credential/accesstoken?grant_type=client_credentials");
 			HttpsURLConnection connection = (HttpsURLConnection) url.openConnection();
 			connection.setRequestMethod("GET");
 			connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
-			connection.setRequestProperty("Autorization",
-					"Basic " + Base64.encode("FMkOk7RUZaNLX6LPVGi6LjsZfBvq8UPu:ALOG1TPrz6hoPmvz".getBytes()));
+			connection.setRequestProperty("Authorization", "Basic " + Base64.getEncoder()
+					.encodeToString("FMkOk7RUZaNLX6LPVGi6LjsZfBvq8UPu:ALOG1TPrz6hoPmvz".getBytes()));
 			connection.setRequestProperty("Accept", "application/json");
 			connection.setDoOutput(true);
 
@@ -39,6 +40,25 @@ public class DailyPracticeExample {
 
 			try (BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));) {
 
+				String line = null;
+				while ((line = br.readLine()) != null) {
+					System.out.println(line);
+				}
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		try {
+			HttpsURLConnection connection = (HttpsURLConnection) new URL(
+					"https://qa.api.tigo.com/v1/tigo/home/work-orders/work-order/MILRM0000000038/1").openConnection();
+			connection.setRequestMethod("GET");
+			connection.setRequestProperty("Authorization", "Bearer CA479GNpgGcDF8VKhMRWI0Bks7Aw");
+
+			try (BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
 				String line = null;
 				while ((line = br.readLine()) != null) {
 					System.out.println(line);
