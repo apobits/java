094c79dca72c98803afa63ecd3c902d75573da8f
aposo <aposo@DESKTOP-SO8KKCH>
2019-04-02 08:28:23 -0500

    udpated

--- JavaPractice/src/practice/java/nio/DailyPracticeExample.java
+++ JavaPractice/src/practice/java/nio/DailyPracticeExample.java
@@ -3,12 +3,19 @@
  */
 package practice.java.nio;
 
+import java.time.Instant;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.time.MonthDay;
+import java.time.OffsetDateTime;
+import java.time.OffsetTime;
 import java.time.Year;
 import java.time.YearMonth;
+import java.time.ZonedDateTime;
 import java.time.temporal.ChronoField;
 import java.time.temporal.TemporalAdjusters;
+import java.util.TimeZone;
 
 /**
  * @author apobits@gmail.com
@@ -27,6 +34,15 @@ public class DailyPracticeExample {
 		System.out.println(YearMonth.now().isLeapYear());
 		System.out.println(MonthDay.now());
 		System.out.println(Year.now());
+
+		System.out.println("LocalDateTime " + LocalDateTime.now());
+
+		System.out.println("LocalTime " + LocalTime.now());
+		System.out.println("ZoneDateTime " + ZonedDateTime.now());
+		System.out.println("OffsetDateTime " + OffsetDateTime.now());
+		System.out.println("OffsetTime " + OffsetTime.now());
+		
+		System.out.println(Instant.now());
 	}
 
 }
