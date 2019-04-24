ec1cfff644c52797047f5335756b8fe9f6929329
apobits <apobits@gmail.com>
2019-04-24 18:05:30 -0500

    updated

--- JavaPractice/src/practice/java/nio/FileVisitorExample.java
+++ JavaPractice/src/practice/java/nio/FileVisitorExample.java
@@ -4,9 +4,11 @@
 package practice.java.nio;
 
 import java.io.IOException;
+import java.nio.file.FileSystems;
 import java.nio.file.FileVisitResult;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.PathMatcher;
 import java.nio.file.Paths;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
@@ -18,16 +20,21 @@ import java.nio.file.attribute.BasicFileAttributes;
 public class FileVisitorExample {
 
 	static class CustomFileVisitor extends SimpleFileVisitor<Path> {
+
+		private PathMatcher pathMatcher = FileSystems.getDefault().getPathMatcher("glob:*.java");
+
 		@Override
 		public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-			System.out.println(file);
+			if (pathMatcher.matches(file.getFileName())) {
+				System.out.println(file);
+			}
 			return FileVisitResult.CONTINUE;
 		}
 
 		@Override
 		public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
-			if (dir.getFileName().toString().equals("marc")) {
-				return FileVisitResult.SKIP_SUBTREE;
+			if (dir.getFileName().toString().equals("bb")) {
+				return FileVisitResult.SKIP_SIBLINGS;
 			} else {
 				return FileVisitResult.CONTINUE;
 			}
@@ -36,7 +43,7 @@ public class FileVisitorExample {
 
 	public static void main(String[] args) {
 		try {
-			Files.walkFileTree(Paths.get("C:\\Users\\Administrador\\Desktop"), new CustomFileVisitor());
+			Files.walkFileTree(Paths.get("C:\\Users\\Administrador\\Desktop\\practice"), new CustomFileVisitor());
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
--- JavaPractice/src/practice/java/varieties/LiteralExample.java
+++ JavaPractice/src/practice/java/varieties/LiteralExample.java
@@ -23,9 +23,31 @@ public class LiteralExample {
 		float float2 = 0F;
 		float float3 = 0;
 		float float4 = 0_1;
+		float float5 = 0b01010;
+//		float float6 = 0b01010f; invalid
+//		float float7 = 0b01010d; invalid
+	}
+
+	public static void doubleLiteral() {
+		double d = 123_123F;
+//		double d1 = 0b101D; invalid
+	}
+
+	public static void intLiteral() {
+		int i = 0b01010100101;
+		System.out.println(i);
+	}
+
+	public static void longLiteral() {
+		long l = 0b111;
+		long ll = 0b111L;
+		long lll = 0b111l;
+		long llll = 0b111;
 	}
 
 	public static void main(String[] args) {
+		intLiteral();
+		System.exit(1);
 		byteLiteral();
 
 	}
