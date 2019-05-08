380998d725e72f2a3551872f82069761dbd1f353
aposo <aposo@DESKTOP-SO8KKCH>
2019-05-08 13:33:40 -0500

    updated

--- JavaPractice/src/practice/java/nio/DailyPracticeExample.java
+++ JavaPractice/src/practice/java/nio/DailyPracticeExample.java
@@ -3,27 +3,18 @@
  */
 package practice.java.nio;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
-import java.util.ServiceLoader;
-
 ;
 
 /**
  * @author apobits@gmail.com
- * @param <T>
+ * 
  *
  */
-public class DailyPracticeExample<T> {
+public class DailyPracticeExample {
 
-	public static void main(String... args) {
-		
-		List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7);
-		numbers.set(0,9);
-		System.out.println(numbers);
-		
-		Optional<Integer> optInt = 	Optional.of(1);
-	
-	} 
+	public static void main(String args[]) {
+
+		var s = new String();
+	}
+
 }
