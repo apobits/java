bdc5970097a730303095ff77e25cc13ac0b49ad4
aposo <aposo@DESKTOP-SO8KKCH>
2019-05-15 08:12:46 -0500

    updated

---
+++ JavaPractice/src/practice/java/varieties/OperatorHierarchicalOrder.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package practice.java.varieties;
+
+/**
+ * @author aposo
+ *
+ */
+public class OperatorHierarchicalOrder {
+
+	public static void main(String[] args) {
+
+		// left to right order
+		System.out.println(5 / 2 * 2);
+		System.out.println(5 + 2);
+		System.out.println(5 << 1);
+		System.out.println(5 >> 1);
+		System.out.println(10 <= 5);
+		System.out.println(Integer.valueOf(1) instanceof Integer);
+
+	}
+
+}
