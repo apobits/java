80b1e5346b332a7df08343ddd74fb830af8527be
apobits <apobits@gmail.com>
2019-05-24 18:06:01 -0500

    updated

---
+++ JavaPractice/src/practice/java/varieties/LoggerExample.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package practice.java.varieties;
+
+import java.util.logging.Logger;
+
+/**
+ * @author apobits@gmail.com
+ *
+ */
+public class LoggerExample {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		Logger.getGlobal().info("Hello from logger");
+
+	}
+
+}
