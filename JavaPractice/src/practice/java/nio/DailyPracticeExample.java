f4d3aac02733d907e5ccd8fbbb6f011cb6a03ff5
apobits <apobits@gmail.com>
2019-05-28 17:40:14 -0500

    updated

--- JavaPractice/src/practice/java/nio/DailyPracticeExample.java
+++ JavaPractice/src/practice/java/nio/DailyPracticeExample.java
@@ -10,7 +10,28 @@ package practice.java.nio;
  */
 class DailyPracticeExample {
 
+	private static class Box<T> {
+
+		T t;
+
+		public void set(T t) {
+			this.t = t;
+		}
+
+		public T get() {
+			return t;
+		}
+	}
+
 	public static void main(String args[]) {
+
+		Box<String> boxS = new Box<>();
+		Box box = boxS;
+		box.set(2);
+		boxS=box;
+		
+		System.out.println(boxS.get());
+		
 
 	}
 
