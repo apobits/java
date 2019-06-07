7e5297663c56c83869eb5641ee1a50aacb508bbf
apobits <apobits@gmail.com>
2019-06-07 18:00:48 -0500

    updated

--- JavaPractice/src/practice/java/nio/DailyPracticeExample.java
+++ JavaPractice/src/practice/java/nio/DailyPracticeExample.java
@@ -3,10 +3,13 @@
  */
 package practice.java.nio;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.TreeMap;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+
+import justdo.it.ocpjp8.Person;
 
 /**
  * @author apobits@gmail.com
@@ -17,13 +20,13 @@ class DailyPracticeExample {
 
 	public static void main(String args[]) {
 
-		List<Integer> list = Stream.of(2, 1, 3).<List<Integer>>collect(() -> new ArrayList<>(), (t, s) -> t.add(s),
-				(t, s) -> t.addAll(s));
+		var persons = Stream.of(new Person("Albin", "Perez", 16055133, 37),
+				new Person("Danna", "Perez", 1055751348, 14), new Person("Marcela", "Castro", 24347671, 38),
+				new Person("Bryan", "Perez", 1111111, 19), new Person("Nidia", "Ortiz", 24835353, 70),new Person("Albin", "Perez", 16055133, 37));
 
-		List<Integer> list1 = Stream.of(2, 1, 3).collect(Collectors.toList());
+		var personsMap = persons
+				.collect(Collectors.toMap(Person::getName, t -> Collections.singleton(t), (t, s) -> {var linkedList = new HashSet<>(t);linkedList.addAll(s);return linkedList;}, TreeMap::new));
+		System.out.println(personsMap);
 
-		System.out.println(list);
-
-		System.out.println(list1);
 	}
 }
