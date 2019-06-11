812baf67e72faa1833e9b9d0d1fcc80a2f78be8e
apobits <apobits@gmail.com>
2019-06-11 18:00:10 -0500

    UPDATED

--- JavaPractice/src/practice/java/nio/DailyPracticeExample.java
+++ JavaPractice/src/practice/java/nio/DailyPracticeExample.java
@@ -3,8 +3,11 @@
  */
 package practice.java.nio;
 
-import java.io.PrintWriter;
-import java.nio.charset.Charset;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
 
 /**
  * @author apobits@gmail.com
@@ -14,11 +17,14 @@ import java.nio.charset.Charset;
 class DailyPracticeExample {
 
 	public static void main(String args[]) {
+		var file = new File("C:\\Users\\Administrador\\Desktop\\Data.txt");
 
-		try (PrintWriter printWriter = new PrintWriter(System.out, false, Charset.forName("UTF-8"))) {
-			printWriter.write('a');
-			printWriter.append("Lolol");
-			printWriter.printf("testing %d", 1);
+		try (var isr = new FileReader(file); var osw = new OutputStreamWriter(System.out)) {
+			isr.transferTo(osw);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 	}
 }
