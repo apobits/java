f4b81a49f8cbc6154237ccb57b2a3bd298fda5cc
apobits <apobits@gmail.com>
2019-06-14 18:10:18 -0500

    update

--- JavaPractice/src/practice/java/nio/DailyPracticeExample.java
+++ JavaPractice/src/practice/java/nio/DailyPracticeExample.java
@@ -4,10 +4,12 @@
 package practice.java.nio;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.OutputStreamWriter;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 /**
  * @author apobits@gmail.com
@@ -17,14 +19,19 @@ import java.io.OutputStreamWriter;
 class DailyPracticeExample {
 
 	public static void main(String args[]) {
-		var file = new File("C:\\Users\\Administrador\\Desktop\\Data.txt");
 
-		try (var isr = new FileReader(file); var osw = new OutputStreamWriter(System.out)) {
-			isr.transferTo(osw);
+		try (ZipOutputStream zos = new ZipOutputStream(
+				new FileOutputStream(new File("C:\\Users\\Administrador\\Desktop\\myZIP.zip")));
+				var fis = new FileInputStream("C:\\\\Users\\\\Administrador\\\\Desktop\\\\SP_COLLECT_EQUIPMENT.sql")) {
+			var entry = new ZipEntry("SP_COLLECT_EQUIPMENT.sql");
+			zos.putNextEntry(entry);
+			zos.write(fis.readAllBytes());
+			zos.closeEntry();
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+
 	}
 }
