59cb00b1c336904f35611389f40fa7eec95d86fb
aposo <aposo@DESKTOP-SO8KKCH>
2019-06-27 23:28:21 -0500

    updated

--- JavaPractice/src/practice/java/nio/DailyPracticeExample.java
+++ JavaPractice/src/practice/java/nio/DailyPracticeExample.java
@@ -3,6 +3,10 @@
  */
 package practice.java.nio;
 
+import java.util.List;
+
+import justdo.it.ocpjp8.Person;
+
 /**
  * @author apobits@gmail.com
  * 
@@ -11,7 +15,7 @@ package practice.java.nio;
 class DailyPracticeExample {
 
 	public static void main(String args[]) {
-		var hex = 0x4D2;
-		System.out.println(hex);
+		var personList = List.of(new Person("Albin", "Perez", 1, 37), new Person("Albin", "Perez", 1, 37));
+		System.out.println(personList.stream().mapToInt(p -> p.getAge()).reduce((t, u) -> t + u).getAsInt());
 	}
 }
