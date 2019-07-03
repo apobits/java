74490324fb6c7ac7c32e46cf1eb9909d54fc42bb
apobits <apobits@gmail.com>
2019-07-03 18:08:48 -0500

    updated

--- JavaPractice/src/practice/java/nio/DailyPracticeExample.java
+++ JavaPractice/src/practice/java/nio/DailyPracticeExample.java
@@ -3,9 +3,9 @@
  */
 package practice.java.nio;
 
-import java.util.List;
-
-import justdo.it.ocpjp8.Person;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 /**
  * @author apobits@gmail.com
@@ -15,7 +15,14 @@ import justdo.it.ocpjp8.Person;
 class DailyPracticeExample {
 
 	public static void main(String args[]) {
-		var personList = List.of(new Person("Albin", "Perez", 1, 37), new Person("Albin", "Perez", 1, 37));
-		System.out.println(personList.stream().mapToInt(p -> p.getAge()).reduce((t, u) -> t + u).getAsInt());
+		
+		System.out.println(System.getProperty("user.dir"));
+		File file = new File("C:\\Users\\Administrador\\Desktop\\dam.txt");
+
+		try (FileOutputStream fos = new FileOutputStream(file)) {
+			fos.write("Works!!! ".getBytes());
+		} catch (IOException e) {
+			System.out.println(e);
+		}
 	}
 }
