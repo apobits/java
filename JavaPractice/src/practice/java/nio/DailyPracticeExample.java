195d34e382d375033fb1106c6213afcb27875e28
aposo <aposo@DESKTOP-FR63375>
2019-08-09 11:08:31 -0500

    updated

--- JavaPractice/src/practice/java/nio/DailyPracticeExample.java
+++ JavaPractice/src/practice/java/nio/DailyPracticeExample.java
@@ -4,42 +4,25 @@
 package practice.java.nio;
 
 import java.io.IOException;
-import java.nio.ByteBuffer;
-import java.nio.channels.FileChannel;
-import java.nio.file.FileSystems;
+import java.lang.reflect.Field;
+
+import practice.java.varieties.Person;
 
 /**
  * @author apobits@gmail.com
  * 
  *
  */
+
 class DailyPracticeExample {
 
-	public static void main(String args[]) throws IOException {
-
-		try (FileChannel fileChannel = FileChannel
-				.open(FileSystems.getDefault().getPath("C:\\Users\\aposo\\Desktop\\x.txt"))) {
-
-			var buffer = ByteBuffer.allocate((int) fileChannel.size());
-			System.out.println(fileChannel.read(buffer));
-			buffer.rewind();
-			while (buffer.hasRemaining()) {
-				System.out.print((char) buffer.get());
-			}
-			buffer.rewind();
-			while (buffer.hasRemaining()) {
-				char temp = (char) buffer.get();
-				if (temp == ' ') {
-					break;
-				}
-			}
-			System.out.println();
-			buffer.compact();
-			buffer.limit(buffer.position());
-			buffer.rewind();
-			while (buffer.hasRemaining()) {
-				System.out.print((char) buffer.get());
-			}
+	public static void main(String args[]) throws IOException, NoSuchFieldException, SecurityException,
+			IllegalArgumentException, IllegalAccessException {
+		Person person = new Person("Albin", "Perez");
+		Field[] fields = person.getClass().getDeclaredFields();
+		for (Field field : fields) {
+			field.setAccessible(true);
+			System.out.println(field.get(person));
 		}
 	}
 }
